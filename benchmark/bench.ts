67c2e953a0a8ae3bf2fc39f8498589fe04828d83
Andrey Allyson <andreyuser47@gmail.com>
2025-01-15 07:07:57 -0300

    build a new version of lib

--- benchmark/bench.ts
+++ benchmark/bench.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { Bench } from 'tinybench'
 
 import { plus100 } from '../index.js'
--- simple-test.js
+++ simple-test.js
@@ -1,5 +1,3 @@
-const { plus100 } = require('./index')
+const { times2 } = require('./index')
 
-console.assert(plus100(0) === 100, 'Simple test failed')
-
-console.info('Simple test passed')
+console.log(times2(12))
